4a732b665f8994d53a1090a99a902d18b839a60e
RoAlencar <roalencarx@gmail.com>
2023-03-02 18:24:25 -0300

    Evoluindo as rotas

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.get('/',(req,res) => {
     res.status(200).send('Curso de Node');
 }) 
 
+app.get('/livros/:id', (req,res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
 app.get('/livros',(req,res) => {
     res.status(200).json(livros);
 })
@@ -21,5 +26,15 @@ app.post('/livros',(req,res) => {
     livros.push(req.body);
     res.status(201).send('Livro foi cadastrado com sucesso!');
 })
+
+app.put('/livros/:id', (req,res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app;
