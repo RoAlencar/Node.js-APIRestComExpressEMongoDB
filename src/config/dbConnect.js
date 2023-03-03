0dd26e4b524940a203fb93360b8b4a85ba3e7a1d
RoAlencar <roalencarx@gmail.com>
2023-03-03 16:43:53 -0300

    Incluindo o Schema do banco e refatorando

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://alura:123@node-express.j0g6hpk.mongodb.net/");
+mongoose.connect("mongodb+srv://alura:123@node-express.j0g6hpk.mongodb.net/alura-node");
 
 let db  = mongoose.connection;
 
