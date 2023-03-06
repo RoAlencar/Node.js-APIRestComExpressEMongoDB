af987550fd827089ba297b649442ef6c6d5fa75f
RoAlencar <roalencarx@gmail.com>
2023-03-06 15:44:35 -0300

    Criando consultas diferenciadas

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -60,6 +60,18 @@ class LivroController {
         })
     }
 
+    static listarLivroPorEditora = (req,res) => {
+        const editora = req.query.editora;
+        
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            if(!err){
+                res.status(200).send(livros);
+            } else {
+                res.status(500).send({message: err.message});
+            }
+        })
+    }
+
 }
 
 export default LivroController;
